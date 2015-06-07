4aa470c82b6488863e5ca289347d00f078ef4cd2
bitingteresa <teresa@scientificlee.com>
2015-06-07 13:05:05 -0400

    my-collection controller

--- app/directives/my-collection/index.js
+++ app/directives/my-collection/index.js
@@ -1,17 +1,33 @@
 import angular from 'angular';
 import template from './my-collection.jade';
+import { name as ParseServiceModule } from 'services/parse';
 import './my-collection.scss';
 
 /* @ngInject */
-function gsMyCollectionController () {
+function gsMyCollectionController (ParseService) {
   const dm = this;
-  dm.excellent = ["do not touch the custom directive", "try to push characters in here"];
+  dm.myComicCollection = [];
 
   dm.testing = function () {
     console.log('ello!')
+  }
+
+  dm.getCollection = function () {
+    ParseService.getCollection().success(function(data){
+      dm.myComicCollection = data.results;
+    });
   };
 
+  dm.addComic = function (comicInfo) {
+    ParseService.addComic(comicInfo).success(function(data){
+      dm.myComicCollection.push(comicInfo);
+    });
+  }
+
+  // dm.addComic({"upc": "1234567890", "comment":"slightly used", "description": "test description", "image": "http://i.annihil.us/u/prod/marvel/i/mg/b/c0/554ccb0c7a2c5/portrait_fantastic.jpg", "title": "test title"});
+  dm.getCollection();
   dm.testing();
+  
 }
 
 function gsMyCollection () {
@@ -27,5 +43,5 @@ function gsMyCollection () {
 }
 
 export default angular.module('gstv.directives.my-collection', [
-])
+ParseServiceModule ])
   .directive('gsMyCollection', gsMyCollection);
