0275ad5a91f09383bdb8e220accaff8da1f18896
bitingteresa <teresa@scientificlee.com>
2015-06-08 14:34:21 -0400

    delete post and get still survive

--- app/services/parse/index.js
+++ app/services/parse/index.js
@@ -42,10 +42,11 @@ class ParseService {
   ////////////////////////////////////////////////////
 dispatch (requestType, resource, config) {
    const endpoint = [PARSE_API, resource].join('/');
-   // this._$http.defaults.headers.common['X-Parse-Application-Id'] = 'dtauSPmJtWaPoU4moKlB8cYsaJ9pCOMe9lCJ8uAI';
-   // this._$http.defaults.headers.common['X-Parse-REST-API-Key'] = PARSE_REST_KEY;
-   // this._$http.defaults.headers.common['Content-Type'] = 'application/json';
-   return this._$http[requestType.toLowerCase()](endpoint, {headers: {'X-Parse-Application-Id': 'dtauSPmJtWaPoU4moKlB8cYsaJ9pCOMe9lCJ8uAI', 'X-Parse-REST-API-Key':PARSE_REST_KEY, 'Content-Type': 'application/json'}});
+   this._$http.defaults.headers.common['X-Parse-Application-Id'] = 'dtauSPmJtWaPoU4moKlB8cYsaJ9pCOMe9lCJ8uAI';
+   this._$http.defaults.headers.common['X-Parse-REST-API-Key'] = PARSE_REST_KEY;
+   this._$http.defaults.headers.common['Content-Type'] = 'application/json';
+  return this._$http[requestType.toLowerCase()](endpoint, config);
+   // return this._$http[requestType.toLowerCase()](endpoint, {headers: {'X-Parse-Application-Id': 'dtauSPmJtWaPoU4moKlB8cYsaJ9pCOMe9lCJ8uAI', 'X-Parse-REST-API-Key':PARSE_REST_KEY, 'Content-Type': 'application/json'}});
  }
 }
 ParseService.$inject = ['$http'];
